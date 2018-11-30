99d7b8ec9ef61d9575ca59f605b728531bf2f747
Parth Sharma <parth.sharma@trios.com>
2018-11-29 18:30:37 -0800

    blog

---
+++ Blog/BlogController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Blog.Models;
+using System.Net;
+using System.Web.Script.Serialization;
+
+namespace Blog
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            var json = new WebClient().DownloadString("https://jsonplaceholder.typicode.com/posts");
+
+
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            BlogModels[] posts = js.Deserialize<BlogModels[]>(json);
+
+
+            ViewBag.posts = posts;
+
+
+            return View();
+        }
+
+        [HttpGet]
+        public string post() {
+
+            var id = Request["id"];
+
+            var json = new WebClient().DownloadString("https://jsonplaceholder.typicode.com/posts/"+id);
+
+            return json;
+
+
+
+
+           
+        }
+    }
+}
---
+++ Blog/Models/BlogModels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models
+{
+    public class BlogModels
+    {
+
+        public int userId { get; set; }
+
+        public int id { get; set; }
+
+        public string title { get; set; }
+
+        public string body { get; set; }
+    }
+}
